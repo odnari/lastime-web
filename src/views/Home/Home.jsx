8aee300432db45e452379f8c470d13ab94a632b6
Nick Odnari <nick@odnari.com>
2022-04-06 14:43:40 +0300

    feat: auto-redirect from home

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,7 +1,9 @@
 import Page from '../../components/Page'
 import { useSelector } from 'react-redux'
+import useAuthenticatedRedirect from '../../hooks/useAuthenticatedRedirect'
 
 export default function Home() {
+  useAuthenticatedRedirect()
   const profile = useSelector(state => state.user.profile)
 
   return <Page>{profile ? `Hello, ${profile.username}` : 'Index'}</Page>
